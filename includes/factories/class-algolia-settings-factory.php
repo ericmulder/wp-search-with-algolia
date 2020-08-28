6f38ec8deb8bd8d3b828c09af20354f1ed7fb1fa
Richard Aber <richard.aber@webdevstudios.com>
2020-08-28 15:26:42 -0500

    #87 Add Algolia_Settings_Factory

---
+++ includes/factories/class-algolia-settings-factory.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Algolia_Settings_Factory class file.
+ *
+ * @since   1.4.1-dev
+ * @package WebDevStudios\WPSWA
+ */
+
+/**
+ * Class Algolia_Settings_Factory
+ *
+ * Responsible for creating a shared instance of the Algolia_Settings object.
+ *
+ * @since 1.4.1-dev
+ */
+class Algolia_Settings_Factory {
+
+	/**
+	 * Create and return an instance of the Algolia_Settings.
+	 *
+	 * @author Richard Aber <richard.aber@webdevstudios.com>
+	 * @since  1.4.1-dev
+	 *
+	 * @return Algolia_Settings The shared Algolia_Settings instance.
+	 */
+	public static function create(): Algolia_Settings {
+
+		/**
+		 * The static instance to share, else null.
+		 *
+		 * @since 1.4.1-dev
+		 *
+		 * @var null|Algolia_Settings $settings
+		 */
+		static $settings = null;
+
+		if ( null !== $settings ) {
+			return $settings;
+		}
+
+		$settings = new Algolia_Settings();
+
+		return $settings;
+	}
+}
